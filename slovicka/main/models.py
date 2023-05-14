b8e78a957ffe6c265e909c1e362702dfa9cb9815
Egor <egor.chekunov@student.gyarab.cz>
2023-05-14 19:14:32 +0200

    save

--- slovicka/main/models.py
+++ slovicka/main/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class Pair(models.Model):
     statement = models.TextField(max_length=64)
     definition = models.TextField(max_length=64)
+    
+    def __str__(self):
+        return f"{self.statement} : {self.definition}"
 
+        
+    
 class Dictionary(models.Model):
     pairs = models.ManyToManyField(Pair, related_name="pairs")
