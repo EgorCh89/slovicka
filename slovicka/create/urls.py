f0677de1068cef881c362969ce0b596aeb11f789
Egor <egor.chekunov@student.gyarab.cz>
2023-05-15 22:11:18 +0200

    udelat new dict

--- slovicka/create/urls.py
+++ slovicka/create/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('edit/<str:name>', views.edit, name='index'),
+    path('new/', views.create_new, name='new'),
 ]
--- slovicka/create/views.py
+++ slovicka/create/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
-
+from django import forms
+import datetime
+from  page.models import Dictionary, Pair
+class NewDictForm(forms.Form):
+    name = forms.CharField(label="Name",max_length=65)
+    public = forms.BooleanField(label="public",required=False)
 # Create your views here.
 def index(request):
-    return render(request,'create/index.html',{
-        
-    })
+    return render(request,'create/index.html')
+
+def create_new(request):
+    if request.method == "POST":
+        form = NewDictForm(request.POST)
+        if form.is_valid():
+            name = form.changed_data['name']
+            pub = form.changed_data['public']
+            dict = Dictionary(name=name,date=datetime.datetime.now(), public=pub,creator_id=request.session.user.id)
+            print(request.session.user.id)
+            return render (request,f"create/edit/{name}",{
+                # and object of dict
+            })
+        else:
+            return render(request,'create/new.html',{
+            'form': NewDictForm(request.POST),
+            })
+    if request.method == "GET":
+        return render(request,'create/new.html',{
+            'form': NewDictForm(),
+        })
+def edit(request,name):
+    if request.method == "GET":
+        return None
