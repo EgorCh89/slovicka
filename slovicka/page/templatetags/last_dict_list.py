ed6e8a628869c0a01a56f3d3b21d58e52a279342
Egor <egor.chekunov@student.gyarab.cz>
2023-05-22 18:56:22 +0200

    rabotaet

--- slovicka/page/templatetags/last_dict_list.py
+++ slovicka/page/templatetags/last_dict_list.py
@@ -1,8 +1,15 @@
 from django import template
 from page.models import *
-from views import recent_dicts
 register = template.Library()
 
 @register.simple_tag
-def get_my_dicts(requset):
-    return recent_dicts(requset)
+def get_my_dicts(user_id):
+    if not user_id:
+        raise template.TemplateSyntaxError('naval id')
+    print(Dictionary.objects.all().filter(creator_id=user_id))
+    a = Dictionary.objects.all().filter(creator_id=user_id).order_by('date').reverse()
+    b = ""
+    for obj in a:
+        b+=f"{obj.name}"
+        b+="\n"
+    return (b)
