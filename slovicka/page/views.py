898d2e867f2458f8c795e5b8c4f60bd2808dac15
Egor <egor.chekunov@student.gyarab.cz>
2023-05-25 22:31:10 +0200

    yes

--- slovicka/page/views.py
+++ slovicka/page/views.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import AnonymousUser, User
 # Create your views here.
 from .models import *
 def index(request):
-    if request.user != AnonymousUser():
-        print(request.user.id)
     return render(request,'page/index.html',{
         'user': request.user,
         "registrated": not request.user == AnonymousUser(),
